446901e029f8abe5a4981787f335b3b50d3a3911
tatiana ramirez <tatiana.w@hotmail.com>
2021-02-20 19:56:38 -0500

    last details

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.15.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "WillJohnson"
-set :repo_url, "https://github.com/willJacct/Rocket_Elevators_Information_System.git"
+set :repo_url, "https://github.com/tatiana-wag/Rocket_Elevators_Information_System.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
