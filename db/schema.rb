25b644b59282dc0002fb63c525efcac28d8538a1
Don <don_nguyen2001@yahoo.com>
2021-02-22 18:32:26 -0800

    create Columns table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_222645) do
+ActiveRecord::Schema.define(version: 2021_02_22_223730) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "address_type"
+    t.string "address_status"
+    t.string "address_entity"
+    t.string "address_street"
+    t.string "address_suite_or_apt"
+    t.string "address_city"
+    t.string "address_zip_code"
+    t.string "address_country"
+    t.text "address_notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "buildings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "building_customer_ID"
+    t.string "building_address"
+    t.string "building_admin_full_name"
+    t.integer "building_admin_phone_number"
+    t.string "building_technical_contact_full_name"
+    t.string "building_technical_contact_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "customer_user_id"
@@ -24,27 +49,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_222645) do
     t.string "customer_full_name_of_service_technical_authority"
     t.string "customer_technical_authority_phone"
     t.string "customer_technical_manager_email"
-  end
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "address_type"
-    t.string "address_status"
-    t.string "address_entity"
-    t.string "address_street"
-    t.string "address_suite_or_apt"
-    t.string "address_city"
-    t.string "address_zip_code"
-    t.string "address_country"
-    t.text "address_notes"
-  end
-
-  create_table "buildings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "building_customer_ID"
-    t.string "building_address"
-    t.string "building_admin_full_name"
-    t.integer "building_admin_phone_number"
-    t.string "building_technical_contact_full_name"
-    t.string "building_technical_contact_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
