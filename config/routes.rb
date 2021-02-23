8672eede5f49ec3dac5254a08c4a48a02c1151db
tatiana ramirez <tatiana.w@hotmail.com>
2021-02-22 19:57:29 -0500

    rebase to e0c571a to rollback delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :quotes
+  resources :leads
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
 
   get "/"  => "pages#index"
 
+  post "index" => "leads#create"
+  
   root to:'pages#index'
 
   devise_scope :user do
