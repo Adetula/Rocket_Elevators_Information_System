7081cffd69e1904ff63cc8603252c1dd3974abdc
Don <don_nguyen2001@yahoo.com>
2021-02-26 18:11:27 -0800

    update

--- Querries/Question1.sql
+++ Querries/Question1.sql
@@ -1,7 +1,13 @@
-select concat(to_char(to_timestamp (extract(month from dwh_created_at)::text, 'MM'), 'TMMonth'), ' ', extract(year from dwh_created_at)) as MonthYearStr, 
+-- select concat(to_char(to_timestamp (extract(month from dwh_created_at)::text, 'MM'), 'TMMonth'), ' ', extract(year from dwh_created_at)) as MonthYearStr, 
+--     count(*) as Nbcontact,
+-- 	concat(extract(year from dwh_created_at),' ', to_char(to_timestamp (extract(month from dwh_created_at)::text, 'MM'), 'TMMM')) as YearMonthInt
+-- from fact_contacts
+-- group by MonthYearStr, YearMonthInt
+-- order by YearMonthInt;
+select concat(to_char(to_timestamp (extract(month from created_on)::text, 'MM'), 'TMMonth'), ' ', extract(year from created_on)) as MonthYearStr, 
     count(*) as Nbcontact,
-	concat(extract(year from dwh_created_at),' ', to_char(to_timestamp (extract(month from dwh_created_at)::text, 'MM'), 'TMMM')) as YearMonthInt
-from fact_contacts
+	concat(extract(year from created_on),' ', to_char(to_timestamp (extract(month from created_on)::text, 'MM'), 'TMMM')) as YearMonthInt
+from factcontact 
 group by MonthYearStr, YearMonthInt
 order by YearMonthInt;
 
--- Querries/Question2.sql
+++ Querries/Question2.sql
@@ -1,6 +1,13 @@
-select concat(to_char(to_timestamp (extract(month from dwh_creation_at)::text, 'MM'), 'TMMonth'), ' ', extract(year from dwh_creation_at)) as MonthYearStr, 
-    count(*) as NbQuotes,
-	concat(extract(year from dwh_creation_at),' ', to_char(to_timestamp (extract(month from dwh_creation_at)::text, 'MM'), 'TMMM')) as YearMonthInt
-from fact_quotes 
+-- select concat(to_char(to_timestamp (extract(month from dwh_creation_at)::text, 'MM'), 'TMMonth'), ' ', extract(year from dwh_creation_at)) as MonthYearStr, 
+--     count(*) as NbQuotes,
+-- 	concat(extract(year from dwh_creation_at),' ', to_char(to_timestamp (extract(month from dwh_creation_at)::text, 'MM'), 'TMMM')) as YearMonthInt
+-- from fact_quotes 
+-- group by MonthYearStr, YearMonthInt
+-- order by YearMonthInt;
+
+select concat(to_char(to_timestamp (extract(month from created_on)::text, 'MM'), 'TMMonth'), ' ', extract(year from created_on)) as MonthYearStr, 
+    count(*) as ContactPerMonth,
+	concat(extract(year from created_on),' ', to_char(to_timestamp (extract(month from created_on)::text, 'MM'), 'TMMM')) as YearMonthInt
+from factquotes 
 group by MonthYearStr, YearMonthInt
 order by YearMonthInt;
